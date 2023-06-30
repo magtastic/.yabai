8a8c260ec556771e6441893211732cddb5c51684
Magnús Ólafsson <magnusol93@gmail.com>
2023-06-30 16:46:01 +0000

    simplify

--- destory_empty_spaces.sh
+++ destory_empty_spaces.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 yabai -m query --spaces --display | \
-     jq -re 'map(select(."native-fullscreen" == 0)) | length > 1' \
+     jq -re 'map(select(."is-native-fullscreen" == false)) | length > 1' \
      && yabai -m query --spaces | \
-          jq -re 'map(select(."windows" == [] and ."focused" == 0).index) | reverse | .[] ' | \
+          jq -re 'map(select(."windows" == [] and ."has-focus" == false).index) | reverse | .[] ' | \
           xargs -I % sh -c 'yabai -m space % --destroy'
